6bb36f90f23aeb56e896de609bbaea8b706b3cdb
Divya-Darshan <parukrish.darshan20@gmail.com>
2026-07-18 20:17:54 +0100

    init repo

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    println!(
+r#"FAD - Fast Audio Director
+
+Usage:
+    fad <command> [options]
+
+Common Commands:
+    play        Play or resume playback
+    pause       Pause playback
+    next        Play the next track
+    previous    Play the previous track
+    status      Show current playback status
+    search      Search YouTube
+    tabs        List browser tabs
+    help        Show help information
+
+For more information on a command:
+    fad help <command>
+
+Project:
+    https://github.com/divya-darshan/fad
+"#);
+}
