decdf18b71f9a73b7039f5fd2dbc2bdff58819fa
Divya Darshan <parukrish.darshan20@gmail.com>
2026-08-04 13:43:12 +0530

    the satrt cmd can satrt barave

--- src/commands/start.rs
+++ src/commands/start.rs
@@ -1,5 +1,17 @@
-pub fn run() { 
+use std::process::{Command};
 
-    println!(" Starting browser ");
+
+pub fn run() { 
+    // define the path to Brave this is mine 🤓☝️
+       let brave_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe";
+   
+       // launch Brave with remote debugging enabled
+       let mut child = Command::new(brave_path)
+           .arg("--remote-debugging-port=9222")
+           .arg("--remote-allow-origins=*")
+           .spawn()
+           .expect("Failed to start Brave Browser");
+   
+       println!("Brave started with PID: {}", child.id());
 
 }
